ff871c6d58e046929540b9ddfe200f78e8dfddab
iuuyen2307 <107293084+iuuyen2307@users.noreply.github.com>
2022-06-11 11:58:56 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    titleWeb: "Thihiiêu đềa web",
+    introTitle: 'Tên người yêu bạn',uyen
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
